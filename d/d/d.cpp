9dbcae7a7880e5bfbcb45387978479910f844c02
煌里 田中 <kmm2459321@stu.o-hara.ac.jp>
2024-07-01 09:52:08 +0900

    BubbleSortSample

---
+++ d/d/d.cpp
@@ -0,0 +1,13 @@
+int main(void) {
+	//struct student snk;
+
+	//snk.height = 175;// 身長 
+
+	//printf("身長＝%d\n", snk.height);
+
+	int age = 10; //年齢
+	if (age <= 12 || num >= 60)
+		printf("%dは奇数で3の倍数です。\n", num);
+	else
+		printf("%dは奇数で３の倍数ではありません。\n", num);
+}
